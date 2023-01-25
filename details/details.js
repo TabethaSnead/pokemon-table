44b597cb5c68bcb5725c6cf3dd1a54129d43cf68
TabethaSnead <tabetha.snead@gmail.com>
2023-01-24 17:59:14 -0800

    deleted console logs

--- details/details.js
+++ details/details.js
@@ -6,9 +6,7 @@ const detailPokemon = document.getElementById('detail-pokemon');
 window.addEventListener('load', async () => {
     const data = new URLSearchParams(window.location.search);
     const id = data.get('id');
-    console.log(id);
     const pokemon = await getPokemonById(id);
-    console.log(pokemon);
     const pokemonDetail = renderPokemonDetail(pokemon[0]);
     detailPokemon.append(pokemonDetail);
 });
